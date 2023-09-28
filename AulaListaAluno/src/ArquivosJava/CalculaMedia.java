ba8fb8439dca3567e24d48e7988515a858f98b2e
EmanuelVitor <emanuellvitor500@gmail.com>
2023-09-28 15:56:43 -0300

    Consertei :)

--- AulaListaAluno/src/ArquivosJava/CalculaMedia.java
+++ AulaListaAluno/src/ArquivosJava/CalculaMedia.java
@@ -12,5 +12,6 @@ public class CalculaMedia {
     public static void main(String[] args) {
         UsuarioDados a1 = new UsuarioDados();
         a1.setNotas(SetCom(1.5f, 1.4f, 1.6f, 1.3f));
+        UsuarioDados a2 = new UsuarioDados();
     }
 }
