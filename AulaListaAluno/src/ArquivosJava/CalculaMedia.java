2a0c75105835f7f14bf0ba2270069e6d9d91df6e
EmanuelVitor <emanuellvitor500@gmail.com>
2023-09-28 15:47:45 -0300

    Estou consertando
    Teste

---
+++ AulaListaAluno/src/ArquivosJava/CalculaMedia.java
@@ -0,0 +1,16 @@
+package ArquivosJava;
+
+import java.util.Arrays;
+
+
+public class CalculaMedia {
+    
+    public static float[] SetCom(float n1, float n2, float n3, float n4) {
+        float[] temp = {n1,n2,n3,n4};
+        return temp;
+    }
+    public static void main(String[] args) {
+        UsuarioDados a1 = new UsuarioDados();
+        a1.setNotas(SetCom(1.5f, 1.4f, 1.6f, 1.3f));
+    }
+}
---
+++ AulaListaAluno/src/ArquivosJava/UsuarioDados.java
@@ -0,0 +1,60 @@
+package ArquivosJava;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class UsuarioDados {
+    //Criando atributos
+    private String nome;
+    private String email;
+    private String senha;
+    private String login;
+    private float[] notas = new float[4];
+    
+    //Definindo funções Get (receber) e Set (definir)
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+    
+    public float[] getNotas() {
+        return notas;
+    }
+
+    public void setNotas(float[] notas) {
+        this.notas = notas;
+    }    
+    
+    public void cadastrar() throws IOException {
+        File lista = new File("Alunos.csv");
+        FileWriter arquivo;
+    }
+}
